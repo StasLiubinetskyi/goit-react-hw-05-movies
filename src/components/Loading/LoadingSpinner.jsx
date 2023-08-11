1148d4a82b17174a8f0fab2fda08206b69edb880
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-08-11 17:43:50 +0300

    index

--- src/components/Loading/LoadingSpinner.jsx
+++ src/components/Loading/LoadingSpinner.jsx
@@ -1,40 +1,11 @@
 import React from 'react';
+import { LoadingSpinnerContainer, Spinner } from './LoadingSpinnerStyled';
 
 const LoadingSpinner = () => {
   return (
-    <div
-      className="loading-spinner"
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      }}
-    >
-      <div
-        className="spinner"
-        style={{
-          border: '4px solid rgba(0, 0, 0, 0.1)',
-          borderLeftColor: '#000',
-          borderRadius: '50%',
-          width: '40px',
-          height: '40px',
-          animation: 'spin 1s linear infinite',
-        }}
-      ></div>
-      <style>
-        {`
-          @keyframes spin {
-            0% {
-                transform: rotate(0deg);
-            }
-            100% {
-                transform: rotate(360deg);
-            }
-          }
-        `}
-      </style>
-    </div>
+    <LoadingSpinnerContainer>
+      <Spinner />
+    </LoadingSpinnerContainer>
   );
 };
 
