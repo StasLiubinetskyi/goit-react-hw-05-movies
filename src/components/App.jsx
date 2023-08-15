154cab972eff81bf0bc43b1b80dcf19384985c3e
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-08-15 22:23:52 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoadingSpinner from './Loading/LoadingSpinner';
-import Layout from './Layout/Layout';
-import NotFound from './NotFound/NotFound';
 
+const Layout = React.lazy(() => import('./Layout/Layout'));
 const Home = React.lazy(() => import('./Home/Home'));
 const Movies = React.lazy(() => import('./Movies/Movies'));
 const MovieDetails = React.lazy(() => import('./MovieDetails/MovieDetails'));
 const Cast = React.lazy(() => import('./Cast/Cast'));
 const Reviews = React.lazy(() => import('./Reviews/Reviews'));
+const NotFound = React.lazy(() => import('./NotFound/NotFound'));
 
 export const App = () => {
   return (
@@ -17,9 +17,10 @@ export const App = () => {
         <Suspense fallback={<LoadingSpinner />}>
           <Layout>
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/movies" element={<Movies />} />
-              <Route path="/movies/:movieId" element={<MovieDetails />}>
+              <Route path="/" element={<Layout />}></Route>
+              <Route index element={<Home />} />
+              <Route path="movies" element={<Movies />} />
+              <Route path="movies/:movieId" element={<MovieDetails />}>
                 <Route path="cast" element={<Cast />} />
                 <Route path="reviews" element={<Reviews />} />
               </Route>
