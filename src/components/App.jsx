738aca9b6a93d3ae6c3af1d937958df58818b545
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-08-16 13:48:20 +0300

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import React, { Suspense } from 'react';
+import { React, Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LoadingSpinner from './Loading/LoadingSpinner';
 
-const Layout = React.lazy(() => import('./Layout/Layout'));
-const Home = React.lazy(() => import('./Home/Home'));
-const Movies = React.lazy(() => import('./Movies/Movies'));
-const MovieDetails = React.lazy(() => import('./MovieDetails/MovieDetails'));
-const Cast = React.lazy(() => import('./Cast/Cast'));
-const Reviews = React.lazy(() => import('./Reviews/Reviews'));
-const NotFound = React.lazy(() => import('./NotFound/NotFound'));
+const Layout = lazy(() => import('./Layout/Layout'));
+const Home = lazy(() => import('./Home/Home'));
+const Movies = lazy(() => import('./Movies/Movies'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const NotFound = lazy(() => import('./NotFound/NotFound'));
 
 export const App = () => {
   return (
